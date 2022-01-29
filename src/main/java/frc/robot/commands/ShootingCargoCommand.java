409bb243995107daa33ef4905abf18e4d6ddef53
Harper Stumb <hdstumb@gmail.com>
2022-01-29 14:02:29 -0500

    Checked work and made minor changes

--- src/main/java/frc/robot/commands/ShootingCargoCommand.java
+++ src/main/java/frc/robot/commands/ShootingCargoCommand.java
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 /** An example command that uses an example subsystem. */
 public class ShootingCargoCommand extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final Shooter m_drive;
+  private final Shooter m_shooter;
 
   /**
    * Creates a new ShootingCargoCommand.
@@ -15,7 +15,7 @@ public class ShootingCargoCommand extends CommandBase {
    * @param subsystem The subsystem used by this command.
    */
   public ShootingCargoCommand(Shooter subsystem) {
-    m_drive = subsystem;
+    m_shooter = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
