892b70d3806956a1a7b83575ca2aa3b634cb0ecd
Harper Stumb <hdstumb@gmail.com>
2022-02-02 19:59:16 -0500

    Added <nearly> complete Climbing subsystem with the relevant command, uses 2 talons, final values still needed

--- src/main/java/frc/robot/commands/ClimberExtendCommand.java
+++ src/main/java/frc/robot/commands/ClimberExtendCommand.java
@@ -1,11 +1,21 @@
 package frc.robot.commands;
 
-
+import java.lang.module.ModuleDescriptor.Requires;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-
+import edu.wpi.first.wpilibj2.command.Subsystem;
 /** An example command that uses an example subsystem. */
 public class ClimberExtendCommand extends CommandBase {
+  
+  
+  
+  
+  @Override
+  public void initialize() {
+
+  }
+
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final Climbing m_climbing;
 
@@ -22,7 +32,23 @@ public class ClimberExtendCommand extends CommandBase {
 @Override
 public void execute() {
   //Write code for climbing here
+  Climbing.ClimberExtendCommand();
+}
+@Override
+public boolean isFinished() {
+  return false;
 }
 
+@Override
+public
+ void end(boolean interrupted) {
+  Climbing.stopClimbing();
+}
+
+//@Override
+//protected void interrupted() {
 
 }
+
+//}
+
