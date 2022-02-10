4fb9e85c0d65cc2f11af6ad2575c2e88e2b4c48d
Terry Phillips <terrydphillips@gmail.com>
2022-02-09 19:17:56 -0500

    Fixed branch build issues

--- src/main/java/frc/robot/commands/FlushIndexerCommand.java
+++ src/main/java/frc/robot/commands/FlushIndexerCommand.java
@@ -1,16 +1,16 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.TimedCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.FlushConstants;
 import frc.robot.Robot;
 import frc.robot.subsystems.Intake;
 
 public class FlushIndexerCommand extends CommandBase {
+  private final Intake m_intake;
+    public FlushIndexerCommand(Intake subsystem) {
 
-  public FlushIndexerCommand() {
     m_intake = subsystem;
-    addRequirments(subsytem); // indexer is needed to intake the ball
+    addRequirements(subsystem); // indexer is needed to intake the ball
   }
 
   public void initialize() {
@@ -29,9 +29,10 @@ public class FlushIndexerCommand extends CommandBase {
   //public boolean isFinished() {
   //    return isTimedOut();
   //return false;
+
+    protected void end() {
+      Intake.stop();
+  }
+    protected void interrupted() {
+  }
 }
-//protected void end() {
-//    Intake.stop();
-//}
-//protected void interrupted() {
-//}
